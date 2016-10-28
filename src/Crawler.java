1ca79056734136f34849a80edbd27af8e4a888ba
Marco Morelos <Marco Morelos>
2016-10-28 16:52:05 -0700

    Now takes page storage folder as input at arg[3]. Creates the folder if it doesnt already exist.

--- src/Crawler.java
+++ src/Crawler.java
@@ -2,68 +2,86 @@ import java.io.*;
 import java.nio.file.*;
 import java.util.Scanner;
 import java.util.concurrent.*;
+import java.net.URL.*;
 
 //we need jsoup library for html parsing
 //Java.net.URL also useful
 
 public class Crawler {
     
+    //argument variables
+    private static Path seedPath;
+    private static int numPagesToCrawl;
+    private static int numLevels;
+    private static Path storagePath;
+    
+    //URL queue. Many threads will access it.
+    private static ConcurrentLinkedQueue<String> frontier = new ConcurrentLinkedQueue<String>();
+    
 	public static void main(String[] args){
 	    
-	    //This is our URL queue
-	    //Initialize it with a seed and watch it go!
-	    ConcurrentLinkedQueue<String> frontier;
-	    frontier = new ConcurrentLinkedQueue<String>();
+	    //initializing the variables
+	    seedPath = null;
+	    numPagesToCrawl = 0;
+	    numLevels = 0;
+	    storagePath = null;
 	    
-	    //argument variables
-	    Path path = null;
-	    int numPagesToCrawl = 0;
-	    int numLevels = 0;
-	    
-	    //set the argument variables
-	    if(args.length == 3){
-            try{
-                path = Paths.get(args[0]);
-	            numPagesToCrawl = Integer.parseInt(args[1]);
-	            numLevels = Integer.parseInt(args[2]);
-            }catch(NumberFormatException e){
-	            e.printStackTrace();
-	        }catch(InvalidPathException e){
-	            e.printStackTrace();
-	        }
-
-	    }
-	    //prints error message if arguments are wrong
-	    else{
-	        System.out.println("Incorrect arguments passed.\n"
-	                + "Arguments are of the form: \n"
-	                + "[Seed file path] [# Pages to Crawl] [# of Levels]");
+	    //prints error message if arguments are wrong then exits
+	    if(args.length < 3 || args.length > 4){
+	        System.out.println("Incorrect arguments passed. Arguments are of the form: \n"
+                    + "[Seed file path] [# Pages to Crawl] [# of Levels][optional: page storage path]");
             return;
 	    }
 	    
-	    //prints out the arguments
-	    System.out.println("path: " + path);
-	    System.out.println("Number of Pages to Crawl: " + numPagesToCrawl);
-	    System.out.println("Number of levels: " + numLevels);
+	    //sets the variables to the arguments
+        try {
+            seedPath = Paths.get(args[0]);
+	        numPagesToCrawl = Integer.parseInt(args[1]);
+	        numLevels = Integer.parseInt(args[2]);
+	        
+	        if(args.length ==  4) storagePath = Paths.get(args[3]);
+            else storagePath = Paths.get("./crawledPages");
+	        
+	        File dir = storagePath.toFile();
+            if(dir.mkdirs()) System.out.println("Storage folder successfully created");
+            else if(!dir.exists()){
+                System.out.println("Storage folder creation failed. Exiting...");
+                return;
+            }
+        }catch(NumberFormatException e){
+	        System.out.println("Are arg[1] and arg[2] numbers?");
+            e.printStackTrace();
+	    }catch(InvalidPathException e){
+	        System.out.println("Invalid path");
+	        e.printStackTrace();
+	    }
+        
+	    ////prints out the variables
+	    //System.out.println("Seed File: " + seedPath);
+	    //System.out.println("Number of Pages to Crawl: " + numPagesToCrawl);
+	    //System.out.println("Number of levels: " + numLevels);
+	    //System.out.println("Storage Path: " + storagePath);
 	    
 	    //initialize the frontier
 	    Scanner s = null;
         try {
-            s = new Scanner(path);
+            s = new Scanner(seedPath);
             while(s.hasNext()){
                 String URL = s.next();
                 frontier.add(URL);
             }
-            s.close();
-            
         } catch (IOException e) {
             e.printStackTrace();
+        } finally {
+            s.close();
         }
         
         //prints the contents of frontier
-        System.out.println("\nSeeds:");
+        System.out.println("Seeds:");
 	    for(String URL : frontier){
 	        System.out.println(URL);
 	    }
+	    
+	    
 	}
 }
