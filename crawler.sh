fe335d6ad9c0f0c262f48eb3d04e50e0c7cde922
knels012 <knels012@ucr.edu>
2016-11-01 05:26:32 -0700

    Update crawler.sh

--- crawler.sh
+++ crawler.sh
@@ -1,9 +1,4 @@
 #!/bin/bash
 # Web Crawler bash script
 
-# DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
-# seed_file="$1"
-# ls "$DIR"
-# seed_file="seed.txt"
-# java -jar CS172_Web_Crawler.jar "$seed_file" "$2" "$3" "$4"
 java -jar CS172_Web_Crawler.jar "$@"
